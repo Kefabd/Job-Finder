85e2066c4d13d73db50fe89d0b57740d3a0b47c5
MohamedZAIM4 <zaim0632@gmail.com>
2025-03-07 10:35:33 +0100

    Avatar Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,18 +16,28 @@ import { HomeComponent } from './home/home.component';
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
-    provideFirebaseApp(() => initializeApp({"projectId":"myjobfinder-93bd0","appId":"1:225733513014:web:071dbb30a6fb5b69252d97","storageBucket":"myjobfinder-93bd0.firebasestorage.app","apiKey":"AIzaSyAn0Jc-VyzfXO7UDZr2ZujKF0gssSdZRyE","authDomain":"myjobfinder-93bd0.firebaseapp.com","messagingSenderId":"225733513014"})),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'myjobfinder-93bd0',
+        appId: '1:225733513014:web:071dbb30a6fb5b69252d97',
+        storageBucket: 'myjobfinder-93bd0.firebasestorage.app',
+        apiKey: 'AIzaSyAn0Jc-VyzfXO7UDZr2ZujKF0gssSdZRyE',
+        authDomain: 'myjobfinder-93bd0.firebaseapp.com',
+        messagingSenderId: '225733513014',
+      })
+    ),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
